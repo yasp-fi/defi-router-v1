26c343c0f4f0d90c231a683e024f55ca1c0707b6
danil-sergeev <cz.gorudo@gmail.com>
2023-05-14 00:46:47 +0100

    chore: json rpc routes layout

--- applications/server/src/app/routes/root.ts
+++ applications/server/src/app/routes/root.ts
@@ -1,10 +1,34 @@
 import { FastifyInstance, FastifyRequest, FastifyReply } from 'fastify';
+import { JsonRpcServer } from "@yasp/utils";
+
+const jsonRpcServer = new JsonRpcServer();
+
+
+// Pricing
+jsonRpcServer.registerProcedure('getAllPriceFeeds', async () => {})
+
+// Infra
+jsonRpcServer.registerProcedure('doHealthCheck', async () => {})
+
+// Info / Data
+jsonRpcServer.registerProcedure('getAllPools', async () => {})
+jsonRpcServer.registerProcedure('getAllProtocols', async () => {})
+
+// Approve
+jsonRpcServer.registerProcedure('getRouterApproveAddress', async () => {})
+jsonRpcServer.registerProcedure('getRouterApprovePayload', async () => {})
+jsonRpcServer.registerProcedure('getRouterCurrentSpendAllowance', async () => {})
+
+// DeFi Invest
+jsonRpcServer.registerProcedure('getDeFiTransactionPayload', async () => {})
+
+
 
 export default async function (fastify: FastifyInstance) {
-  fastify.get(
-    '/',
+  fastify.post(
+    '/jsonRpc',
     async function (request: FastifyRequest, reply: FastifyReply) {
-      return { message: 'Hello API' };
+      return jsonRpcServer.handleFastifyIncomingRequest(request, reply);
     }
   );
 }
